95cefd8e89fb9e1649525e773c25d16afd2449ce
A2drien <adriendubois1411@gmail.com>
2022-06-20 00:14:50 +0200

    Petite mise en forme du code

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from urllib.parse import urljoin
 import pandas as pd
@@ -9,8 +10,8 @@ def get_liste_url_departement() -> list[str]:
     """Retourne la liste des URLs de tous les départements"""
 
     soup = BeautifulSoup(requests.get(URL).content, "html.parser")
-    select_tag :Tag = soup.find("select")  #type: ignore
-    options :ResultSet[Tag] = select_tag.find_all("option")
+    select_tag: Tag = soup.find("select")  #type: ignore
+    options: ResultSet[Tag] = select_tag.find_all("option")
 
     return [urljoin(URL, "./" + (str(option["value"]))) for option in options][1:]
 
@@ -18,18 +19,21 @@ def get_liste_url_departement() -> list[str]:
 def get_liste_url_circonscription(url_departement: str) -> list[str]:
     """Retourne la liste des URLs de toutes les circonscriptions du département"""
 
-    if url_departement == EXCEPTION_DEP_99:
-        soup = get_reponse_url(url_departement)
-        select_tag = soup.find("table", {"class" : "table table-bordered"}) #type: ignore
-        liste_circo = [urljoin(url_departement, link.get('href')) for link in select_tag.find_all('a')]  # type: ignore
-    
+    #Si le département actuel est le 75, alors procéder à la sélection "à la main"
     if url_departement == EXCEPTION_DEP_75:
-        liste_circo = [f"https://www.resultats-elections.interieur.gouv.fr/legislatives-2022/075/075{conversion_nombre(i)}.html" for i in range(1,19)]  # type: ignore
+        liste_circo = [f"https://www.resultats-elections.interieur.gouv.fr/legislatives-2022/075/075{conversion_nombre(i)}.html" for i in range(1,19)] 
         return liste_circo
+
     
     soup = get_reponse_url(url_departement)
-    select_tag = soup.find_all("div", {"class" : "offset2 span8"}) #type: ignore
-    liste_circo = [urljoin(url_departement, link.get('href')) for link in select_tag[1].find_all('a')]
+
+    #Si le département est celui des francais de l'étranger, alors procéder à un 
+    if url_departement == EXCEPTION_DEP_99:
+        select_tags: ResultSet[Tag] = soup.find("table", {"class": CLASSE_TABLE_EXCEPTION_DEP_99}) #type: ignore
+        liste_circo = [urljoin(url_departement, link.get('href')) for link in select_tags.find_all('a')]  #type: ignore
+    
+    select_tags: ResultSet[Tag] = soup.find_all("div", {"class": CLASSE_DIVISION})
+    liste_circo = [urljoin(url_departement, link.get('href')) for link in select_tags[1].find_all('a')]
 
     return liste_circo
 
@@ -41,24 +45,25 @@ def get_resultats(url_circonscription: str)-> list[dict[str, str]]:
     soup = get_reponse_url(url_circonscription)
 
     try:
-        select_tag :Tag = soup.find_all("table",{"class":CLASSE_TABLEAU})  #type: ignore
-        select_tag :Tag = select_tag[-2].find("tbody")  # type: ignore
+        select_tags: ResultSet[Tag] = soup.find_all("table",{"class": CLASSE_TABLE_RESULTATS})
+        select_tag: Tag = select_tags[-2].find("tbody") #type: ignore
 
-        lignes :ResultSet[Tag] = select_tag.find_all("tr")
+        lignes: ResultSet[Tag] = select_tag.find_all("tr")
 
         for ligne in lignes:
             donnes_candidat = {}
-            data_brutes :ResultSet[Tag] = ligne.find_all("td")
+            donnees_brutes: ResultSet[Tag] = ligne.find_all("td")
             
-            donnes_candidat[LABEL_PRENOM], donnes_candidat[LABEL_NOM] = traitement_prenom_nom(data_brutes[IDX_NOM_PRENOM].text)
-            donnes_candidat[LABEL_NB_VOIX] = "".join(data_brutes[IDX_NB_VOIX].text.split())
-            donnes_candidat[LABEL_CIRCONSCRIPTION] = traitement_circonscription(url_circonscription)
+            donnes_candidat[LABEL_PRENOM], donnes_candidat[LABEL_NOM] = traitement_prenom_nom(donnees_brutes[IDX_NOM_PRENOM].text)
+            donnes_candidat[LABEL_NB_VOIX] = "".join(donnees_brutes[IDX_NB_VOIX].text.split())
+            donnes_candidat[LABEL_CIRCONSCRIPTION] = get_circonscription(url_circonscription)
             
             liste_donnes_candidats.append(donnes_candidat)
 
         return liste_donnes_candidats
 
     except:
+        print(f"Résultats manquants pour la circonscription {get_circonscription(url_circonscription)}")
         return [{}]
 
 
--- methods_utils.py
+++ methods_utils.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup
 URL = "https://www.resultats-elections.interieur.gouv.fr/legislatives-2022/index.html"
 
 NOM_FICHIER = "data.csv"                            #Nom du fichier où sauvegarder les données
-CLASSE_TABLEAU = "tableau-resultats-listes-ER"      #Nom de la classe du tableau web à scrapper
+CLASSE_TABLE_RESULTATS = "tableau-resultats-listes-ER"      #Nom de la classe du tableau web à scrapper
+CLASSE_TABLE_EXCEPTION_DEP_99 = "table table-bordered"
+CLASSE_DIVISION = "offset2 span8"
 
 IDX_NOM_PRENOM = 0                                  #Index de la colonne où trouver le nom et prénom du candidat
 IDX_NB_VOIX = 2                                     #Index de la colonne où trouver le nombre de voix qu'à obtenu un candidat
@@ -27,10 +29,10 @@ def get_reponse_url(url: str) -> BeautifulSoup:
     return BeautifulSoup(requests.get(url).text, "html.parser")
 
 
-def traitement_prenom_nom(prenom_nom :str) -> tuple[str, str]:
+def traitement_prenom_nom(prenom_nom: str) -> tuple[str, str]:
     """
     Prend le nom/prénom ent paramètre et retourne le nom et le prénom
-    Ex : 'M. Jean BON LE GRAND' renverra : 'Jean', 'BON LE GRAND'
+    Ex: 'M. Jean BON LE GRAND' renverra : 'Jean', 'BON LE GRAND'
     """
 
     liste_prenom_nom = prenom_nom.split()[1:]       #On enlève le M./Mme
@@ -42,7 +44,7 @@ def traitement_prenom_nom(prenom_nom :str) -> tuple[str, str]:
     return prenom, nom
 
 
-def traitement_circonscription(url_circonscription: str) -> str:
+def get_circonscription(url_circonscription: str) -> str:
     """Prend une URL en paramètre et retourne la circonscription au format xxx-yy"""
     
     format = url_circonscription[-10:-5]
@@ -55,6 +57,8 @@ def traitement_circonscription(url_circonscription: str) -> str:
     return num_departement + "-" + num_circonscription
 
 
-def conversion_nombre(nombre :int) -> str:
+def conversion_nombre(nombre: int) -> str:
+    """Retourne un chiffre en format XX au de lieu de X"""
+
     if nombre < 10: return "0" + str(nombre)
     else: return str(nombre)
